e0597fa3025481e8bba6ce352eb79084184460e3
jairo Rodrigues <jairo.goncalves90@gmail.com>
2017-11-20 23:22:52 -0200

    aula 2 finalizada

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = require('./config/express')();
 
-app.get('/produtos',function(req,res){
-    res.render("produtos/lista")
-});
-
 app.listen(3000, function(){
     console.log("Servidor rodando");
 });
