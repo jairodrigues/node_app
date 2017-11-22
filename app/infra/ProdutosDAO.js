140341819afcf3f0a5a918a41032575cb09594cd
jairo Rodrigues <jairo.goncalves90@gmail.com>
2017-11-21 23:58:05 -0200

    verbos http

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -6,6 +6,14 @@ ProdutosDAO.prototype.lista = function(callback){
     this._connection.query('select * from livros', callback);
 }
 
+ProdutosDAO.prototype.salva = function(produto,callback){
+    this._connection.query('insert into livros set ?',produto, callback);
+}
+
+ProdutosDAO.prototype.delete = function(produtos, callback){
+    this._connection.query('delete from livros where ' + id, callback);
+}
+
 module.exports = function(){
   return ProdutosDAO;
 };
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,37 @@
 module.exports = function(app){
-    app.get("/produtos",function(req,res){
-        
+   var listaProdutos = function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
-
-        produtosBanco.lista(function(err, results){
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function(err, results){
             res.render('produtos/lista', { lista: results });
         });
         connection.end();
+    };
+
+    app.get('/produtos',listaProdutos);
+
+    app.get('/produtos/form', function(req,res){
+        res.render('produtos/form');
     });
+
+    app.post('/produtos', function(req,res){
+        var produto = req.body;
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function(erros,resultados){
+            res.redirect('/produtos');
+        });
+    });
+
+    app.get('/produtos/:id', function(req, res){
+        var id = req.params.id;
+        id = id.replace(":","");
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.delete(id, function(erros, resultados){
+            res.redirect('/produtos');
+        })
+    })
 
 }
   
